fb693a84aed66a7f2e11401cd9ae7556765b3f02
ArtemissFR <artemissfr@noreply.gitea.com>
2024-01-31 15:12:56 +0000

    Actualiser Cisco_Switch/Ping_Test.sh

--- Cisco_Switch/Ping_Test.sh
+++ Cisco_Switch/Ping_Test.sh
@@ -1,2 +1,7 @@
-ansible Routers -m ping -u cisco -k
-echo 'Ping has been executed on the remote system !'
+ansible Routers -m ping -u cisco -k # Command to ping the remote systems.
+echo 'Ping has been executed on the remote system !' 
+
+#[Template]
+# ansible [target] -m ping -u [user] -k
+# echo 'Ping has been executed on the remote system !'
+#[/Template]
