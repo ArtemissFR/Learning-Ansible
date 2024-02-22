b3a3516b16adecc6a7d8f59b0a76776dba6d0c07
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-22 15:18:01 +0100

    Delete Cisco_Switch/Ping_Test.sh

--- Cisco_Switch/Ping_Test.sh
+++
@@ -1,7 +0,0 @@
-ansible Routers -m ping -u cisco -k # Command to ping the remote systems.
-echo 'Ping has been executed on the remote system !' 
-
-#[Template]
-# ansible [target] -m ping -u [user] -k
-# echo 'Ping has been executed on the remote system !'
-#[/Template]
